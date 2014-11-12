df10e3dc0a706b3f9124c55954208514f846c435
Neel Smith <dnsmith.neel@gmail.com>
2014-11-12 16:07:17 -0500

    Work on automated md->concordion.

--- specs/specs/oldpersian/transliteration/NonAlphabeticTest.java
+++ specs/specs/oldpersian/transliteration/NonAlphabeticTest.java
@@ -8,15 +8,14 @@ import edu.holycross.shot.oldpersian.OPTransliteration;
 import org.concordion.integration.junit3.ConcordionTestCase;
 
 public class NonAlphabeticTest extends ConcordionTestCase {
-    
-    public String xlitPunct(String op) {
-	StringBuffer buff = new StringBuffer(op);
-	int   codePoint = buff.codePointAt(0);
-	int max = buff.codePointCount(0, buff.length() - 1);
-	    
-	System.err.println ("Total cps: " + max + ". First code point " + codePoint);
-	return OPTransliteration.uToXLit(op);
 
+
+    public String getLogoXlit(String op) {
+	return OPTransliteration.uToXLit(op);
+    }
+    
+    public String getPunctXlit(String op) {
+	return OPTransliteration.uToXLit(op);
     }
 
     public String cuneiformPunct(String xlit) {
