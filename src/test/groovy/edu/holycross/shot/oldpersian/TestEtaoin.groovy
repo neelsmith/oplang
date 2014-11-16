8657713bc5ff576c99000fc4354e5ad1f22e374d
Neel Smith <dnsmith.neel@gmail.com>
2014-11-16 07:48:00 -0500

    Add test data.

--- src/test/groovy/edu/holycross/shot/oldpersian/TestEtaoin.groovy
+++ src/test/groovy/edu/holycross/shot/oldpersian/TestEtaoin.groovy
@@ -9,15 +9,15 @@ class TestEtaoin extends GroovyTestCase {
   
   void testFile() { 
 
-
-
-    File src = new File("op-etaoin.txt")
-    File outFile = new File("testdata/testout/etaoin-xlit.txt")  
+    File src = new File("testdata/op-etaoin.txt")
+    File outFile = new File("testdata/testout/etaoin-xlit.txt")
+    String expectedXlit = ":a-ya-ma-i-ra-va-u-ta-sha-ha-da"
     Integer lnCount = 0
     src.eachLine { l ->
       lnCount++;
       String xlit = OPTransliteration.uToXLit(l) 
       outFile.append( "${xlit}" ,"UTF-8")
+      assert xlit == expectedXlit
     }
     outFile.append("\n")
   }
